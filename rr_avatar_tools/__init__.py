ae56993c21d02c7fdb0cfb592a5253a54280e548
Aurel Lazar <aurel@recroom.com>
2025-10-02 15:39:06 -0700

    Bump RRAT Version -> 1.2.0

--- rr_avatar_tools/__init__.py
+++ rr_avatar_tools/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Rec Room Avatar Tools",
-    "author": "Joshua Skelton",
-    "version": (1, 1, 1),
+    "author": "Rec Room",
+    "version": (1, 1, 2),
     "blender": (4, 2, 0),
     "location": "",
     "description": "",
